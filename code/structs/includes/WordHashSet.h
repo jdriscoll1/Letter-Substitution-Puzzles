4322f91e28d975f482f96e10558f02c7040f5a58
rjd_gamer <jman248@comcast.net>
2021-10-13 16:35:00 -0700

    Adding the new word set functionality

---
+++ code/structs/includes/WordHashSet.h
@@ -0,0 +1,32 @@
+/*
+Title: WordHashSet.h
+Author: Jordan Driscoll
+Date: 10/13/21
+Desc: A Hash Set for all words to determine if they are used
+*/
+
+
+/*This is the storage for words that have and have not been used */
+struct WordHashSet{
+	//the array of all words, each word takes up a byte
+	unsigned long* wordSet; 
+	//the total number of words
+	int totalWords; 
+};
+
+/*Allocates the structure and creates all of the necessary longs*/
+struct WordHashSet* create_WordHashSet(int totalWords);
+
+/*Function that marks a word as used or unused
+@param wordID --> The word that is being marked used or unused
+@param isUsed --> 1 - the word was just seen 0 - the word is being marked as unseen*/
+void markUsed_WordHashSet(int wordID, int isUsed, struct WordHashSet *whs);
+
+/*Check if word is used*/
+void checkIfUsed_WordHashSet(int wordID, struct WordHashSet *whs);
+
+/*Go through all the words and mark them unused*/
+void reset_WordHashSet(int wordID, struct WordHashSet* whs);
+
+/*frees hash set of all words*/
+void free_WordHashSet(struct WordHashSet* whs);
---
+++ code/structs/src/WordHashSet.c
@@ -0,0 +1,39 @@
+/*Title: WordHashSet.c
+Author: Jordan Driscoll
+Date: October 13th, 2021
+Desc: A Hash Set for all words to determine if they are used*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "../includes/WordHashSet.h"
+
+#define NUM_BYTES 8
+
+/*Allocates the structure and creates all of the necessary longs*/
+struct WordHashSet* create_WordHashSet(int totalWords){
+	//the total number of word blocks
+	int arrLength = ceil(totalWords / (sizeof(unsigned long) * NUM_BYTES));
+	
+	//the hash set that contains whether all words have been, or have not been used 
+	struct WordHashSet *whs = calloc(arrLength, sizeof(unsigned long)); 
+	
+	
+}
+
+/*Function that marks a word as used or unused
+@param wordID --> The word that is being marked used or unused
+@param isUsed --> 1 - the word was just seen 0 - the word is being marked as unseen*/
+void markUsed_WordHashSet(int wordID, int isUsed, struct WordHashSet *whs);
+
+/*Check if word is used*/
+void checkIfUsed_WordHashSet(int wordID, struct WordHashSet *whs);
+
+/*Go through all the words and mark them unused*/
+void reset_WordHashSet(int wordID, struct WordHashSet* whs);
+
+/*frees hash set of all words*/
+void free_WordHashSet(struct WordHashSet* whs);
